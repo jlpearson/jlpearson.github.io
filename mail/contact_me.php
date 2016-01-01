96925a1948c158b6cdb0ff4108b29c6f840daf59
InRetentis <Jon@Jons-Air.lan>
2016-01-01 05:03:47 +0100

    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,8 +19,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: " . strip_tags($_POST['email']) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
